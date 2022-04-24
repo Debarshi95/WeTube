b6492da48a3c7b0d2ef97c30ebce870c61cdd1c0
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-04-24 20:06:11 +0530

    Update index files, App.js

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Card } from './Card/Card';
+export { default as SideDrawer } from './SideDrawer/SideDrawer';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Text } from './Text/Text';
+export { default as Image } from './Image/Image';
+export { default as Button } from './Button/Button';
+export { default as ErrorBoundary } from './ErrorBoundary/ErrorBoundary';
+export { default as NotFound } from './NotFound/NotFound';
